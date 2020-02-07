571d2c8a0f6d6e2f38d73cb986cd148d8f308393
dkernGit <snowpunk@gmail.com>
2020-02-06 19:21:44 -0800

    Updates

---
+++ precache-manifest.fa2272d098c728bd8f11b9579a73a0c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95f52bce5aa3dd180f44da6c9f795a4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3a780d41484fc3dff14f",
+    "url": "/monsters-rolodex/static/css/main.b3e1267c.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a780d41484fc3dff14f",
+    "url": "/monsters-rolodex/static/js/main.2e183c59.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
